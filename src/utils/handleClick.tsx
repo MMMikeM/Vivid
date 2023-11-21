bb1f03e0b231c96fd3ae6468913fe4ac56ef4fa3
Mike Murray <mike.murray@peachpayments.com>
2023-11-21 17:50:22 +0200

    fix: 🚨 Move out clickhandler to make linter happy. WIP

---
+++ src/utils/handleClick.tsx
@@ -0,0 +1,18 @@
+import { ApiTreeNode, dictState } from "../store/tree";
+import { getEntries } from "./objects";
+import { HierarchyNode } from "../components/Node";
+
+const updateNode = (
+  node: HierarchyNode,
+  newNode: Omit<Partial<ApiTreeNode>, "node_id">
+) => {
+  const { id } = node.data;
+  for (const [key, value] of getEntries(newNode)) {
+    //@ts-expect-error This works, but the proxy makes the types confused
+    dictState[id][key] = value;
+  }
+};
+
+export const handleClick = (node: HierarchyNode) => {
+  updateNode(node, { name: Math.random().toFixed(2).toString() + " " });
+};

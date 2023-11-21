29b0f8d384a0d77cfe60fa1424edb22ae89c5b23
Mike Murray <mike.murray@peachpayments.com>
2023-11-21 08:56:07 +0200

    feat: 🚧 Creates constants

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+import colors from "tailwindcss/colors";
+const orange = colors.orange[500];
+const pink = colors.pink[500];
+const blue = colors.blue[500];
+const green = colors.green[500];
+const indigo = colors.indigo[500];
+const lightpurple = colors.purple[300];
+const white = colors.white;
+const background = colors.slate[800];
+
+export const theme = {
+  orange,
+  pink,
+  blue,
+  green,
+  indigo,
+  lightpurple,
+  white,
+  background,
+};
